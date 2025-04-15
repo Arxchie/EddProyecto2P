ef0f718190aeba2a1dd184515f7a80ca4fda92d4
Arxchie <jeduardoacacio@gmail.com>
2025-04-15 17:04:12 -0600

    calses base agregadas

---
+++ src/edd_hospital_/modelo/Especialidad.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edd_hospital_.modelo;
+
+/**
+ *
+ * @author saulo
+ */
+public class Especialidad extends Datos
+{
+
+    private int numeroDeCamas;
+    private int numeroDeMedicos;
+     private String claveE;
+
+    public Especialidad(int camas, int medicos, String nombre)
+    {
+        super(nombre);
+        if (camas < 0 || medicos < 0)
+        {
+            throw new IllegalArgumentException("El número de camas y médicos no puede ser negativo");
+        }
+        this.numeroDeCamas = camas;
+        this.numeroDeMedicos = medicos;
+        this.claveE = generarClave('E');
+    }
+
+    /**
+     * @return the numeroDeCamas
+     */
+    public int getNumeroDeCamas()
+    {
+        return numeroDeCamas;
+    }
+
+    /**
+     * @param numeroDeCamas the numeroDeCamas to set
+     */
+    public void setNumeroDeCamas(int numeroDeCamas)
+    {
+        this.numeroDeCamas = numeroDeCamas;
+    }
+
+    /**
+     * @return the numeroDeMedicos
+     */
+    public int getNumeroDeMedicos()
+    {
+        return numeroDeMedicos;
+    }
+
+    /**
+     * @param numeroDeMedicos the numeroDeMedicos to set
+     */
+    public void setNumeroDeMedicos(int numeroDeMedicos)
+    {
+        this.numeroDeMedicos = numeroDeMedicos;
+    }
+
+    /**
+     * @return the claveE
+     */
+    public String getClaveE()
+    {
+        return claveE;
+    }
+
+    /**
+     * @param claveE the claveE to set
+     */
+    public void setClaveE(String claveE)
+    {
+        this.claveE = claveE;
+    }
+
+   
+}
---
+++ src/edd_hospital_/modelo/Hospitales.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edd_hospital_.modelo;
+
+/**
+ *
+ * @author saulo
+ */
+public class Hospitales extends Datos
+{
+
+    private String direccion;
+    private int nivel;
+    private String claveH;
+
+    public Hospitales(String direc, int nivel, String nombre)
+    {
+        super(nombre);
+        this.direccion = direc;
+        this.nivel = nivel;
+        this.claveH = generarClave('H');
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion()
+    {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion)
+    {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the nivel
+     */
+    public int getNivel()
+    {
+        return nivel;
+    }
+
+    /**
+     * @param nivel the nivel to set
+     */
+    public void setNivel(int nivel)
+    {
+        this.nivel = nivel;
+    }
+
+    /**
+     * @return the claveH
+     */
+    public String getClaveH()
+    {
+        return claveH;
+    }
+
+    /**
+     * @param claveH the claveH to set
+     */
+    public void setClaveH(String claveH)
+    {
+        this.claveH = claveH;
+    }
+
+}
